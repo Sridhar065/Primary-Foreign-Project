5068aecd261d7f7d7134ba39d91140ccfbc031f0
Aswin <aswin.san6@gmail.com>
2021-03-05 16:11:27 +0530

    first commit

---
+++ info.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.3
+ */
+
+/**
+ * Database `info`
+ */
+
+/* `info`.`enrol` */
+$enrol = array(
+  array('rol_no' => '3','stud_id' => '1'),
+  array('rol_no' => '4','stud_id' => '2'),
+  array('rol_no' => '5','stud_id' => '3'),
+  array('rol_no' => '6','stud_id' => '4'),
+  array('rol_no' => '7','stud_id' => '5')
+);
+
+/* `info`.`student` */
+$student = array(
+  array('stud_id' => '1','name' => 'SRIDHAR','age' => '22'),
+  array('stud_id' => '2','name' => 'ASWIN','age' => '23'),
+  array('stud_id' => '3','name' => 'VIJAY','age' => '25'),
+  array('stud_id' => '4','name' => 'MANOJ KUMAR','age' => '22'),
+  array('stud_id' => '5','name' => 'ABISHEK','age' => '21')
+);
